b8cedc7ab7649786ff708f2b8413a04f8f1c6482
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-07-12 17:45:50 +0200

    Create a window

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,29 @@
 #include <iostream>
+
 #include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char** argv) {
     // Prepare SPDLOG
     spdlog::set_level(spdlog::level::trace);
     spdlog::set_pattern("%^[%l] %v%$");
 
-    spdlog::trace("Logging prepared.");
+    spdlog::info("Logging prepared.");
+
+    if(!glfwInit()) {
+        spdlog::error("GLFW: could not be initialized");
+        std::exit(1);
+    }
+    spdlog::info("GLFW: initialized");
     
+    GLFWwindow* window = glfwCreateWindow(1000, 1000, "Test", nullptr, nullptr);
+    if(!window) {
+        spdlog::error("GLFW: window could not be created");
+        std::exit(1);
+    }
+    spdlog::info("GLFW: 1000x1000 window created");
+    
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
 }
