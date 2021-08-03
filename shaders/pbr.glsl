ee01e2d4cd13807df96f6181aba0cbbf4c045aed
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-08-03 13:17:43 +0200

    First implementation (not exactly working)

--- shaders/pbr.glsl
+++ shaders/pbr.glsl
@@ -29,6 +29,8 @@ void main()
 #shader frag
 #version 330 core
 
+const float PI = 3.1416;
+
 in vec3 v_normal;
 in vec2 v_texture_coordinates;
 in vec3 v_fragment_position;
@@ -44,6 +46,41 @@ uniform sampler2D normal_texture;
 const float ambient_color = 0.3;
 const vec3 light_direction = vec3(0.0, 1.0, 0.0);
 
+float fresnel_schlick(float cos_theta, float F0) {
+    return F0 + (1.0 - F0) * pow(max(1.0 - cos_theta, 0.0), 5.0);
+}
+
+float distribution_ggx(vec3 N, vec3 H, float a)
+{
+    float a2     = a*a;
+    float NdotH  = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH*NdotH;
+
+    float nom = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+
+    return nom / denom;
+}
+
+float geometry_schlick_ggx(float NdotV, float k)
+{
+    float nom   = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+	
+    return nom / denom;
+}
+  
+float geometry_smith(vec3 N, vec3 V, vec3 L, float k)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx1 = geometry_schlick_ggx(NdotV, k);
+    float ggx2 = geometry_schlick_ggx(NdotL, k);
+	
+    return ggx1 * ggx2;
+}
+
 void main()
 {
     // Gather from the textures all the necessary values
@@ -51,17 +88,26 @@ void main()
     float roughness_value = texture(diffuse_texture, v_texture_coordinates).x;
     vec3 normal_vector = texture(normal_texture, v_texture_coordinates).xyz;
 
-    // Normalize light direction and normal not to mess with lighting
-    vec3 normalized_normal = normalize(v_normal);
-    vec3 normalized_light = normalize(light_direction);
+    vec3 N = normalize(v_normal);
+    vec3 V = normalize(u_camera_position - v_fragment_position);
+    vec3 L = normalize(light_direction);
+    vec3 H = normalize(V + L);
 
-    float diffuse_component = max(dot(normalized_normal, normalized_light), 0.0);
-    
-    vec3 view_direction = normalize(u_camera_position - v_fragment_position);
-    vec3 reflected_direction = reflect(-light_direction, normalized_normal);
-    float shininess = (1 - roughness_value) * 256;
-    float specular_component = pow(max(dot(view_direction, reflected_direction), 0.0), shininess);
+    float fresnel_distribution = fresnel_schlick(max(dot(H, V), 0.0), 0.03);
+    float geometry_distribution = geometry_smith(N, V, L, roughness_value);
+    float normal_distribution = distribution_ggx(N, H, roughness_value);
 
-    vec3 final_color = (ambient_color + diffuse_component + specular_component) * base_color;
-    out_color = vec4(min(final_color, vec3(1.0)), 1.0f);
-} 
+    float numerator = normal_distribution * fresnel_distribution * geometry_distribution;
+    float denominator = 4 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
+
+    float specular_fraction = fresnel_distribution;
+    float diffuse_fraction = 1 - specular_fraction;
+
+    vec3 lambert_diffuse = base_color / PI;
+    float cook_torrance_specular = numerator / denominator;
+
+    vec3 final_fragment = diffuse_fraction * lambert_diffuse + 
+                          specular_fraction * cook_torrance_specular;
+
+    out_color = vec4(final_fragment, 1.0);
+}
